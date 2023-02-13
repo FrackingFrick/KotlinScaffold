c3b42fe8c0dbf532bd820d3255a3cf4654df95ed
FrackingFrick <heta.myllymaki@gmail.com>
2023-02-13 13:17:32 +0200

    The scaffold and navigation exercise

--- app/src/main/java/com/example/scaffold/MainActivity.kt
+++ app/src/main/java/com/example/scaffold/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import com.example.scaffold.ui.theme.ScaffoldTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,22 +36,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ScaffoldApp() {
-    Scaffold (
-        topBar = { MyTopBar()},
-        content= { Text(text = "This is the content")},
-        bottomBar = { BottomAppBar() { Text(text = "Bottom bar")}}
-            )
-
-
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "Home"
+    ) {
+        composable(route = "Home") {
+            MainScreen(navController)
+        }
+        composable(route = "Info") {
+            InfoScreen(navController)
+        }
+        composable(route = "Settings") {
+            SettingsScreen(navController)
+        }
+    }
 }
 @Composable
-fun MyTopBar(){
+fun MyTopBar(title:String, navController: NavController){
     var expanded by remember { mutableStateOf(false) }
     TopAppBar(
-        title={ Text( "My App")},
+        title={ Text(title)},
         navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Filled.MoreVert, contentDescription = null)
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
             }
         },
         actions = {
@@ -56,16 +69,49 @@ fun MyTopBar(){
             DropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded=false }) {
-                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                DropdownMenuItem(onClick = { navController.navigate("info") }) {
                     Text(text = "Info")
                 }
-                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                DropdownMenuItem(onClick = {navController.navigate("settings") }) {
                     Text(text = "Settings")
 
                 }
             }
         }
     )
+}
+@Composable
+fun ScreenTopBar(title:String, navController: NavController) {
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        }
+    )
+}
+@Composable
+    fun MainScreen(navController: NavController) {
+        Scaffold(
+            topBar = { MyTopBar("My App", navController) },
+            content = { Text(text = "This is the homepage content") }
+        )
+    }
+
+@Composable
+fun InfoScreen(navController: NavController) {
+        Scaffold(
+            topBar = { ScreenTopBar("Info", navController) },
+            content = { Text(text = "This is content for info page") }
+        )
+}
+@Composable
+fun SettingsScreen(navController: NavController) {
+        Scaffold(
+            topBar = { ScreenTopBar("Settings", navController) },
+            content = { Text(text = "This is content for settings page") }
+        )
 }
 
 @Preview(showBackground = true)
